09b40097c9ce4c6990b4e8da8a0bd68cdf68a0ba
Nihal Londhe <79393628+nihallondhe@users.noreply.github.com>
2023-06-24 21:34:11 +0530

    deploy nginx create ec2 ssh key security groups

---
+++ ssh-key-pair.tf
@@ -0,0 +1,13 @@
+resource "tls_private_key" "tf_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "tf_key" {
+  key_name   = "tf-test-key"      
+  public_key = tls_private_key.tf_key.public_key_openssh
+
+  provisioner "local-exec" { 
+    command = "echo '${tls_private_key.tf_key.private_key_pem}' > ./tf-test-key"
+  }
+}
